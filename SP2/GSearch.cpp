1040c389d7d67763cfb42f883d3bfc2182df05c6
emisaar <emisaar0406@gmail.com>
2022-11-16 08:26:16 -0600

    GSearch modified

--- SP2/GSearch.cpp
+++ SP2/GSearch.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <iostream>
 
+//Ordenamos los puntos considerando su posición en el eje "x"
 //Complejidad: O(1)
 bool sortByX(Point a, Point b){
     if (a.x == b.x)
@@ -17,6 +18,7 @@ bool sortByX(Point a, Point b){
     return a.x < b.x;
 }
 
+//Ordenamos los puntos considerando su posición en el eje "y"
 //Complejidad: O(1)
 bool sortByY(Point a, Point b){
     if (a.y == b.y)
@@ -24,8 +26,9 @@ bool sortByY(Point a, Point b){
     return a.y < b.y;
 }
 
-//FUNCIÓN CALCULA-DISTANCIA-MÍNIMA
-//Complejidad: O(n(log n) + (log(n))^2)
+//FUNCIÓN CALCULA-DISTANCIA-MÍNIMA: Calcula la menor distancia entre
+//todos los puntos
+//Complejidad: O(n*log(n) + 2*((n/2)^2) + c*log(c) + c^2)
 // 1) Generar lista de puntos
 float GSearch::calculaDistMinima(vector<Point> puntos){
     vector<pair<int, float>> id_dist;
@@ -42,14 +45,14 @@ float GSearch::calculaDistMinima(vector<Point> puntos){
     
     // 4) Todos contra todos
     // 5) Obtener la menor distancia de cada subarreglo
-    //Complejidad: O((log(n))^2)
+    //Complejidad: O((n/2)^2)
     for (int i = 0; i < leftHalf.size(); i++){
         for (int j = i + 1; j < leftHalf.size(); j++){
             float dist = sqrt(pow(leftHalf[j].x - leftHalf[i].x, 2) + pow(leftHalf[j].y - leftHalf[i].y, 2));
             minDistLeft = min(minDistLeft, dist);
         }
     }
-    //Complejidad: O((log(n))^2)
+    //Complejidad: O((n/2)^2)
     for (int i = 0; i < rightHalf.size(); i++){
         for (int j = i + 1; j < rightHalf.size(); j++){
             float dist = sqrt(pow(rightHalf[j].x - rightHalf[i].x, 2) + pow(rightHalf[j].y - rightHalf[i].y, 2));
@@ -79,11 +82,11 @@ float GSearch::calculaDistMinima(vector<Point> puntos){
     }
 
     // 11) Ordenar los puntos con respecto a Y (menor a mayor)
-    //Complejidad: O(n*(log n))
+    //Complejidad: O(c*(log c))
      sort(puntos.begin(), puntos.end(), sortByY);
 
     // 12) Todos contra todos
-    //Complejidad: O(c) --> c < log(n)  --> c = cinta
+    //Complejidad: O(c^2)
     float minCinta = minDist;
     for (int i = 0; i < cinta.size(); i++){
         for (int j = i + 1; j < cinta.size(); j++){
@@ -95,23 +98,42 @@ float GSearch::calculaDistMinima(vector<Point> puntos){
 }
 
 
+//Ordenamos los puntos considerando su posición en el eje "x"
+//Complejidad: O(1)
 bool sortPairByDist(pair<Point, float> a, pair<Point, float> b){
     return a.second < b.second;
 }
 
+//FUNCIÓN CALCULA-DISTANCIA-MÍNIMA: Calcula la menor distancia entre
+//todos los puntos
+//Complejidad: O(n*log(n) + n)
 void GSearch::calculaDistMinima2(vector<Point> puntos){
     vector<Point> comp(puntos.begin(), puntos.end() - 1);
     Point casa = puntos[puntos.size() - 1];
     vector<pair<Point, float>> dists;
-
+    
+    //Complejidad: O(n)
     for (int i = 0; i < comp.size(); i++){
         float dist = sqrt(pow(comp[i].x - casa.x, 2) + pow(comp[i].y - casa.y, 2));
         dists.push_back({comp[i], dist});
     }
     
-    for (int i = 0; i < comp.size(); i++){
-        cout << "\tDistancia de la casa (" << casa.x << ", " << casa.y << ") a la tienda (" << comp[i].x << ", " << comp[i].y << ") es: " << dists[i].second << endl;
+    //Complejidad: O(n*log(n))
+    sort(puntos.begin(), puntos.end(), sortByX);
+    cout << "\tPuntos ordenados de izquierda a derecha: " << endl;
+    int index = 1;
+    //Complejidad: O(n)
+    for (Point p : puntos){
+        cout << "\t" << index++ << ".- (" << p.x  << ", " << p.y << ")" << endl;
     }
+    cout << endl;
+    
+    //Complejidad: O(n)
+    for (int i = 0; i < comp.size(); i++){
+        cout << "\tDistancia de la nueva contratación (" << casa.x << ", " << casa.y << ") a la tienda (" << comp[i].x << ", " << comp[i].y << "): " << dists[i].second << "Km" << endl;
+    }
+    
+    //Complejidad: O(n*log(n))
     sort(dists.begin(), dists.end(), sortPairByDist);
-    cout << "\n\tLa mejor opción es: (" << dists[0].first.x << ", " << dists[0].first.y << ") con una distancia de " << dists[0].second << endl;
+    cout << "\n\tLa central más cercana a la nueva contratación es: (" << dists[0].first.x << ", " << dists[0].first.y << ") con una distancia de " << dists[0].second << "Km" << endl;
 }
